99c1d5956de3af73befc8b72e03f841ae1b0d1a2
fiendpinkman <fiendpinkman@gmail.com>
2026-09-10 21:46:17 +0530

    Add: merge-intervals - submission-4

---
+++ Data Structures & Algorithms/merge-intervals/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        int[][] result = new int[intervals.length][2];
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        int j = 0;
+        int prevStart = intervals[0][0];
+        int prevEnd = intervals[0][1];
+        for (int i=1; i<intervals.length; i++) {
+            int currentStart = intervals[i][0];
+            int currentEnd = intervals[i][1];
+
+            // overlap happening
+            if ((prevStart<=currentStart && currentStart<=prevEnd)) {
+                prevStart = Math.min(prevStart, currentStart);
+                prevEnd = Math.max(prevEnd, currentEnd);
+            } else {
+                result[j][0] = prevStart;
+                result[j][1] = prevEnd; 
+                prevStart = currentStart;
+                prevEnd = currentEnd;
+                j++;
+            }
+        }
+
+        result[j][0] = prevStart;
+        result[j][1] = prevEnd;
+        
+        return Arrays.copyOf(result, j+1);
+    }
+}
