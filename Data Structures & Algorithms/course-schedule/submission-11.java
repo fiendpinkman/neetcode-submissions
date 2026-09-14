26861c7fe87dab2e3e67ee3b94274b7adbaa5d03
fiendpinkman <fiendpinkman@gmail.com>
2026-09-14 16:58:30 +0530

    Add: course-schedule - submission-11

---
+++ Data Structures & Algorithms/course-schedule/submission-11.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, Integer> iMap = new HashMap<>();
+        Map<Integer, List<Integer>> neighbors = new HashMap<>();
+        Queue<Integer> q = new LinkedList<>();
+        for (int[] prerequisite: prerequisites) {
+            iMap.put(prerequisite[1], iMap.getOrDefault(prerequisite[1], 0) + 1);
+            List<Integer> temp = neighbors.getOrDefault(prerequisite[0], new ArrayList<Integer>());
+            temp.add(prerequisite[1]);
+            neighbors.put(prerequisite[0], temp);
+        }
+        
+        for (int[] prerequisite: prerequisites) {
+            if (iMap.get(prerequisite[0]) == null && !q.contains(prerequisite[0])) {
+                q.add(prerequisite[0]);
+            }
+        }
+    
+        while(!q.isEmpty()) {
+            int parent = (int) q.poll();
+            System.out.println("Queue :" + parent);
+            List<Integer> temp = neighbors.get(parent);
+            if (temp!=null) {
+                for (Integer neighbor: neighbors.get(parent)) {
+                    iMap.put(neighbor, iMap.get(neighbor)-1);
+                    if (iMap.get(neighbor)==0) {
+                        q.add(neighbor);
+                        iMap.remove(neighbor);
+                    }
+                }
+            }
+        }
+        
+        return iMap.size()==0;
+    }
+}
